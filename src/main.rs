d1f30e9edd4f6f5cbd325482566ab28cd9c8b54f
Chih-Kai (Patrick) Wang <kk1fff@ckwang.info>
2017-10-14 17:00:58 +0800

    cache computed values

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-pub trait Function {
+use std::collections::HashSet;
+
+trait Function {
     fn derivative_over(&self, node_name: &str) -> f64;
-    fn compute_value(&self) -> f64;
+    fn compute_value(&mut self) -> (Vec<String>, f64);
     fn has_node(&self, node_name: &str) -> bool;
 }
 
@@ -27,8 +29,8 @@ impl Function for ConstVal {
             panic!("Not found: {:?}", node_name);
         }
     }
-    fn compute_value(&self) -> f64 {
-        self.val
+    fn compute_value(&mut self) -> (Vec<String>, f64) {
+        (vec![self.name.clone()], self.val)
     }
     fn has_node(&self, node_name: &str) -> bool {
         self.name == node_name
@@ -39,6 +41,8 @@ impl Function for ConstVal {
 struct AddFunc<Op1: Function, Op2: Function> {
     op1: Op1,
     op2: Op2,
+    val1: f64,
+    val2: f64,
     name: String,
 }
 
@@ -48,6 +52,8 @@ impl <Op1, Op2> AddFunc <Op1, Op2>
         AddFunc {
             op1: op1,
             op2: op2,
+            val1: 0_f64,
+            val2: 0_f64,
             name: String::from(name)
         }
     }
@@ -65,8 +71,13 @@ impl <Op1, Op2> Function for AddFunc <Op1, Op2>
         }
     }
 
-    fn compute_value(&self) -> f64 {
-        self.op1.compute_value() + self.op2.compute_value()
+    fn compute_value(&mut self) -> (Vec<String>, f64) {
+        let (mut op1, val1) = self.op1.compute_value();
+        let (mut op2, val2) = self.op2.compute_value();
+        op1.append(&mut op2);
+        self.val1 = val1;
+        self.val2 = val2;
+        (op1, val1+val2)
     }
 
     fn has_node(&self, node_name: &str) -> bool {
@@ -77,7 +88,10 @@ impl <Op1, Op2> Function for AddFunc <Op1, Op2>
 struct MultiplyFunc<Op1: Function, Op2: Function> {
     op1: Op1,
     op2: Op2,
+    val1: f64,
+    val2: f64,
     name: String,
+    op_set: HashSet<String>,
 }
 
 impl <Op1, Op2> MultiplyFunc <Op1, Op2>
@@ -86,7 +100,10 @@ impl <Op1, Op2> MultiplyFunc <Op1, Op2>
         MultiplyFunc {
             op1: op1,
             op2: op2,
-            name: String::from(name)
+            val1: 0_f64,
+            val2: 0_f64,
+            name: String::from(name),
+            op_set: HashSet::new()
         }
     }
 }
@@ -95,16 +112,21 @@ impl <Op1, Op2> Function for MultiplyFunc <Op1, Op2>
     where Op1: Function, Op2: Function {
     fn derivative_over(&self, node_name: &str) -> f64 {
         if self.op1.has_node(node_name) {
-            self.op2.compute_value() * self.op1.derivative_over(node_name)
+            self.val2 * self.op1.derivative_over(node_name)
         } else if self.op2.has_node(node_name) {
-            self.op1.compute_value() * self.op2.derivative_over(node_name)
+            self.val1 * self.op2.derivative_over(node_name)
         } else {
             panic!("Not found: {:?}", node_name);
         }
     }
 
-    fn compute_value(&self) -> f64 {
-        self.op1.compute_value() * self.op2.compute_value()
+    fn compute_value(&mut self) -> (Vec<String>, f64) {
+        let (mut op1, val1) = self.op1.compute_value();
+        let (mut op2, val2) = self.op2.compute_value();
+        op1.append(&mut op2);
+        self.val1 = val1;
+        self.val2 = val2;
+        (op1, self.val1 * self.val2)
     }
 
     fn has_node(&self, node_name: &str) -> bool {
@@ -122,7 +144,8 @@ fn main() {
     let f1 = AddFunc::new(a, b, "A+B");
     let f2 = MultiplyFunc::new(f1, c, "(A+B)C");
 
-    let f_end = MultiplyFunc::new(f2, d, "(A+B)CD");
-    println!("Val: {:?}", f_end.compute_value());
-    println!("Derive: {:?}", f_end.derivative_over("A"));
+    let mut f_end = MultiplyFunc::new(f2, d, "(A+B)CD");
+    let (_, v) = f_end.compute_value();
+    println!("Val: {:?}", v);
+    println!("Derive: {:?}", f_end.derivative_over("D"));
 }
